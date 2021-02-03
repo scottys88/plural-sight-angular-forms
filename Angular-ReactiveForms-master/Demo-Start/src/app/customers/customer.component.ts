fcc21fe6e2cb38023b0bc5600810553efe9fd106
scottys88 <scott_schubert@hotmail.com.au>
2021-02-03 22:43:07 +1030

    WIP

--- Angular-ReactiveForms-master/Demo-Start/src/app/customers/customer.component.ts
+++ Angular-ReactiveForms-master/Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    console.log(c);
+    if (c.value !== null && (isNaN(c.value)) || c.value < min || c.value > max) {
+      return { range: true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -16,12 +26,39 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit(): void {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['',
+        [
+          Validators.required,
+          Validators.minLength(3)]
+        ],
+      lastName: ['',
+        [
+          Validators.required,
+          Validators.maxLength(50)
+        ]
+      ],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required]
+      }),
+      phone: '',
+      rating: [null, ratingRange(1, 5)],
+      notification: 'email',
       sendCatalog: true
     });
+  }
 
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+
+    if (notifyVia === 'text') {
+      phoneControl.clearValidators();
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 
   populateTestData(): void {
